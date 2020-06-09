4cdd1d9d9a47dbd7eccaa570e7ed1c0fab945345
Realmm <jamesandrew107@hotmail.com>
2020-06-10 09:51:13 +1200

    Added Placeholder

---
+++ src/main/java/net/jamesandrew/realmlib/placeholder/Placeholder.java
@@ -0,0 +1,39 @@
+package net.jamesandrew.realmlib.placeholder;
+
+import net.jamesandrew.commons.logging.Logger;
+
+import java.util.Arrays;
+
+public final class Placeholder {
+
+    private final String main;
+    private String[] placeholders;
+    private String[] toReplace;
+
+    public Placeholder(String main) {
+        this.main = main;
+    }
+
+    public Placeholder setPlaceholders(String... placeholders) {
+        this.placeholders = Arrays.stream(placeholders).map(s -> "%" + s + "%").toArray(String[]::new);
+        Logger.debug(Arrays.stream(this.placeholders));
+        return this;
+    }
+
+    public Placeholder setToReplace(Object... objects) {
+        this.toReplace = Arrays.stream(objects).map(Object::toString).toArray(String[]::new);
+        Logger.debug(Arrays.stream(toReplace));
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        String s = main;
+        if (placeholders.length != toReplace.length) throw new IllegalArgumentException("Array lengths cannot be different");
+        for (int i = 0; i < placeholders.length; i++) {
+            s = s.replace(placeholders[i], toReplace[i]);
+        }
+        return s;
+    }
+
+}
