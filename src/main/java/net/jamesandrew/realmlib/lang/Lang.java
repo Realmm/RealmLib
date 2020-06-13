ae0fcc01fc85dd882087d785cf52818e0b3fb472
Realmm <jamesandrew107@hotmail.com>
2020-06-13 19:22:08 +1200

    Added ability to disable print messages and added TextComponent methods

--- src/main/java/net/jamesandrew/realmlib/lang/Lang.java
+++ src/main/java/net/jamesandrew/realmlib/lang/Lang.java
@@ -1,5 +1,9 @@
 package net.jamesandrew.realmlib.lang;
 
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 
@@ -29,6 +33,20 @@ public final class Lang {
         } else sb.append(color(file.getString(path)));
 
         return sb.toString();
+    }
+
+    public static TextComponent toTextComponent(String s, HoverEvent h, ClickEvent c) {
+        TextComponent t = new TextComponent(s);
+        t.setClickEvent(c);
+        t.setHoverEvent(h);
+        return t;
+    }
+
+    public static TextComponent toClickableText(String s, String cmd, String hoverMsg) {
+        return toTextComponent(s,
+                new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(hoverMsg).create()),
+                new ClickEvent(ClickEvent.Action.RUN_COMMAND, cmd)
+        );
     }
 
 }
